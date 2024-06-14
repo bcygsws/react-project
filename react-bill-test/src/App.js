b19d3de8982289aaf192166b3bd67b213993e551
bcygsws <bcygswspring168@gmail.com>
2024-06-14 20:26:09 +0800

    :memo: 记账单项目开始一

--- react-bill-test/src/App.js
+++ react-bill-test/src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button} from 'antd-mobile';
 
 function App() {
-  return (
-    <div className="App">
-      <h3>这是App组件</h3>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h3>这是App组件</h3>
+			<Button color="primary">这是Button组件</Button>
+		</div>
+	);
 }
 
 export default App;
