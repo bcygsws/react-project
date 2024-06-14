d4be1750c959e8c394e5ba143928810dbede89a4
bcygsws <bcygswspring168@gmail.com>
2024-06-14 21:21:40 +0800

    :memo: 账单项目开始二

--- react-bill-test/src/index.js
+++ react-bill-test/src/index.js
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	// 前已验证，严格模式会导致useEffect中回调执行两次，可暂时关闭"严格模式"
+	<App/>
+	// <React.StrictMode>
+	//   <App />
+	// </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
