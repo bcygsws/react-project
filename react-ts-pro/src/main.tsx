fb06343ff3926a0646145a77233b50c8a70b2c3b
bcygsws <bcygswspring168@gmail.com>
2024-07-19 03:24:15 +0800

    :memo: 解决vite构建的react-ts项目报红问题

--- react-ts-pro/src/main.tsx
+++ react-ts-pro/src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+);
+/**
+ *
+ * @bug:
+ * 创建项目报错后，依照下述文档解决：
+ * https://blog.csdn.net/qq_45862085/article/details/135263134?spm=1001.2101.3001.6650.3&utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7ERate-3-135263134-blog-136801441.235%5Ev43%5Econtrol&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7ERate-3-135263134-blog-136801441.235%5Ev43%5Econtrol&utm_relevant_index=4
+ *
+ * bug1: 当前文件中，React和ReactDOM报红时，
+ * 解决：在tsconfig.node.json和tsconfig.app.json中都添加以下配置，就可以解决
+ * "allowSyntheticDefaultImports": true
+ *
+ * */
