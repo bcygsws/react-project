9373fb5c633eadbb95362522496201ae42747e28
bcygsws <bcygswspring168@gmail.com>
2024-12-30 04:08:02 +0800

    :memo: 关于context上下文实现组件通信

--- react18_learn_v2/src/components/commu_context/Test1.tsx
+++ react18_learn_v2/src/components/commu_context/Test1.tsx
@@ -1,9 +1,34 @@
 import {Component} from "react";
-// 在类组件中使用this.context就可以拿到当前消费的上下文了
 import context from './Context';
 
+/**
+ * @desc:context通信方式的前世今生
+ * 参考文档：https://www.cnblogs.com/davina123/p/13634350.html
+ * 1.context通信，最开始的方式
+ * 联想记忆： childContextTypes记忆起
+ * 需要安装prop-types包，react高版本已经集成这个包，无需单独安装了
+ * 发送数据：
+ * 1.1 静态属性声明，申明要传递的数据的类型
+ * static childContextTypes={
+ *     data:propTypes.string
+ * }
+ * 1.2 明确要发送的数据
+ * getChildContext(){
+ *     return {data:'你好'}
+ * }
+ *
+ * 接收数据：
+ * 1.3 静态属性声明，校验接收的类型
+ * static contextTypes={
+ *    data:propTypes.string
+ * }
+ * 1.4 组件中，this.context.data就可以拿到传递的值
+ *
+ * */
+
 export class Test1 extends Component {
-    // 静态成员contextType不用写this.context中的this;因为它是跟随类创建时候创建出来的，此时还没有this实例
+    // 1.静态成员contextType不用写this.context中的this;因为它是跟随类创建时候创建出来的，此时还没有this实例
+    // 2.将context赋值给contextType以后，在类组件中使用this.context就可以取到传递的值了
     static contextType = context;
 
     render() {
