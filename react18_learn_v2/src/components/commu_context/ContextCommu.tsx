0492151b726eadeffa714b139828feaad9db5a9c
bcygsws <bcygswspring168@gmail.com>
2024-12-29 23:58:14 +0800

    :memo: Context通信的扩展

--- react18_learn_v2/src/components/commu_context/ContextCommu.tsx
+++ react18_learn_v2/src/components/commu_context/ContextCommu.tsx
@@ -1,6 +1,3 @@
-import {useState} from "react";
-import Son from "@/components/commu_context/Son";
-
 /**
  * @desc：八、react中使用上下文context实现各层级组件通信
  * 使用步骤：
@@ -15,8 +12,13 @@ import Son from "@/components/commu_context/Son";
  * const {name}=useContext(OrgContext);
  * name就可以渲染在接受到某个组件内
  *
+ * 注：孙子组件GrandSon中，theme值修改为green,祖父组件中，ContextCommu中，theme值修也同时修改为blue
+ * 4.1 可以这样理解，以setTheme方法是祖父组件的方法，传递给孙子组件了；孙子组件中setTheme调用了，
+ * 那么祖父组件中的值，也会修改为green
+ *
  * */
-
+import {useState} from "react";
+import Son from "@/components/commu_context/Son";
 import GrandContext from './Context.tsx';
 import {Test1} from "@/components/commu_context/Test1";
 
