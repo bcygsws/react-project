b20f397e9115230680aad398de9bdcf2467ecf85
bcygsws <bcygswspring168@gmail.com>
2025-01-31 22:07:16 +0800

    :memo: Json-server的用法

--- react-meituan/src/App.js
+++ react-meituan/src/App.js
@@ -6,7 +6,12 @@ import {useEffect} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import {getFoodList} from "./store/modules/takeaway";
 
-import './App.scss'
+import './App.scss';
+/**
+ * @desc：json=-server的使用详解
+ * 参考文档:https://blog.csdn.net/m0_67444449/article/details/141092022?utm_medium=distribute.pc_relevant.none-task-blog-2~default~baidujs_baidulandingword~default-0-141092022-blog-139607652.235^v43^pc_blog_bottom_relevance_base7&spm=1001.2101.3001.4242.1&utm_relevant_index=3
+ *
+ * */
 
 // const foodsList = [
 // 	{
