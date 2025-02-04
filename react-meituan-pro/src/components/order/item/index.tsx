0de464466b4441f30cfcc59df7ac62f957fc79a8
bcygsws <bcygswspring168@gmail.com>
2025-02-04 21:16:39 +0800

    :art: 路由的配置

--- react-meituan-pro/src/components/order/item/index.tsx
+++ react-meituan-pro/src/components/order/item/index.tsx
@@ -1,13 +1,27 @@
 import './index.scss';
-import {NavLink, Outlet} from "react-router-dom";
+import {NavLink, Outlet, useLocation, useNavigate} from "react-router-dom";
+import {useEffect} from "react";
 
 const Item = ({list}) => {
     console.log('list=======', list);
+    const location = useLocation();
+    console.log("location····", location);
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (location.pathname === '/order') {
+            const obj = list.find((item, index) => index === 0);
+            console.log("obj===", obj);
+            navigate(`/order/${obj.id}`)
+
+        }
+    }, []);
+
     return (
         <div className="bot">
             <div className="bot-left">
                 {
-                    list.map((item) => {
+                    list.map((item, index) => {
+                        console.log(index);
                         return (<NavLink to={`/order/${item.id}`} key={item.id}
                                          style={({isActive}) => ({
                                              backgroundColor: isActive ? '#ffffff' : '#eeeeee',
